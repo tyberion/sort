7fab76b4b06d3e7f01f678eeae8954b1608d9cbc
Maarten Kruithof <maarten.kruithof@tno.nl>
2019-04-26 10:24:53 +0200

    Preparing for packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
-import numpy as np
+#  import numpy as np
+#  
+#  from distutils.core import setup
+#  from Cython.Build import cythonize
+#  
+#  setup(
+#      ext_modules = cythonize('sort_utils.pyx'),
+#      include_dirs = [np.get_include()]
+#  )
 
-from distutils.core import setup
-from Cython.Build import cythonize
+import setuptools
 
-setup(
-    ext_modules = cythonize('sort_utils.pyx'),
-    include_dirs = [np.get_include()]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sort-kruithofmc",
+    version="1.0.0",
+    author="Maarten Kruithof",
+    author_email="maarten.kruithof@tno.nl",
+    description="A python package of the SORT tracking algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tyberion/sort",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
 )
